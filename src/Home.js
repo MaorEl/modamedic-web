0446b203f7e6df201dc540c992541b2021437370
maore <maorelfassy@gmail.com>
2020-05-08 14:35:13 +0300

    fix?

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ class Home extends Component{
 
     render(){
         return(
-            <Router>
+            <Router basename="/modamedic-web/">
                 <Switch>
                     <Route exact path="/">
                         <HomeLogin />
