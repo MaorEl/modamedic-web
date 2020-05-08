97d503b5ce6fb800e96732d18a822ec0cd7921c2
maore <maorelfassy@gmail.com>
2020-05-08 14:18:10 +0300

    Updates

---
+++ precache-manifest.1cf10aab0c87b3cf9afe66ea49955e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff507307c6346feaf89f308391413dc",
+    "url": "/modamedic-web/index.html"
+  },
+  {
+    "revision": "85c82d9324f19415bf45",
+    "url": "/modamedic-web/static/css/main.80c5b720.chunk.css"
+  },
+  {
+    "revision": "701fa06ced01a39bda09",
+    "url": "/modamedic-web/static/js/2.7cca0f85.chunk.js"
+  },
+  {
+    "revision": "ca1a7e1d71f22c9557085fe4aa4cf6ac",
+    "url": "/modamedic-web/static/js/2.7cca0f85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85c82d9324f19415bf45",
+    "url": "/modamedic-web/static/js/main.5527e373.chunk.js"
+  },
+  {
+    "revision": "e23d3a96fe6eb3cc2c2e",
+    "url": "/modamedic-web/static/js/runtime-main.752597ec.js"
+  },
+  {
+    "revision": "4c4417d4b07f7a7d02393cdb9ed86a38",
+    "url": "/modamedic-web/static/media/first_logo.4c4417d4.png"
+  }
+]);
